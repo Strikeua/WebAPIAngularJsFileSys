0422cd52cd666fe09ae7f56336823dda4c1f59ab
STRIKE <STRIKE@HOME>
2016-07-15 13:11:00 +0300

    Create a project
    This is first commit

---
+++ WebAPIAngularJsFileSystem.WebUI/Models/FileViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebAPIAngularJsFileSystem.WebUI.Models
+{
+    public class FileViewModel
+    {
+        public string Name { get; set; }
+        public long Size { get; set; }
+    }
+}
